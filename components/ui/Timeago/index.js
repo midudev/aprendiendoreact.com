2f58df54307336aa5da4dccc7d7ac82773078557
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-02-24 11:33:08 +0100

    Remove console.log

--- components/ui/Timeago/index.js
+++ components/ui/Timeago/index.js
@@ -1,11 +1,8 @@
 import { spanishLocale } from './locale'
 import timeago from 'timeago.js'
 
-console.log(spanishLocale)
 timeago.register('es', spanishLocale)
-console.log(timeago)
 const timeagoInstance = timeago()
-console.log(timeagoInstance)
 
 export function Timeago ({ date }) {
   return timeagoInstance.format(date, 'es')
